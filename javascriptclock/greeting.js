3c7926cd259983934d0263dee353f72f8a120c96
Semanur Çakır <126287184+smnrckr@users.noreply.github.com>
2023-10-09 22:50:41 +0300

    Update greeting.js

--- javascriptclock/greeting.js
+++ javascriptclock/greeting.js
@@ -1,12 +1,12 @@
 let id=prompt("Lütfen adinizi giriniz");
 
 function clock(){
-    let myName=document.querySelector("#myName");
-    myName.innerHTML=id;
+    let myName = document.querySelector("#myName");
+    myName.innerHTML = id;
 
-    let myClock=document.querySelector("#myClock");
-    let date=new Date();
-    let days=["Pazartesi","Sali","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
-    myClock.innerHTML =` ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${days[date.getDay()]}`;
+    let myClock = document.querySelector("#myClock");
+    let date = new Date();
+    let days = ["Pazartesi","Sali","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
+    myClock.innerHTML = ` ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${days[date.getDay()]}`;
 }
 setInterval(clock,1000);
